9a5f83b45c77dec7d13190ce44c1136b0e25e2b3
Avinesh Dasari <Avinesh Dasari@BG1NBR1378.SONATA.LOCAL>
2022-01-21 15:16:27 +0530

    web

---
+++ mywebtest77/src/main/java/com/sonata/App.java
@@ -0,0 +1,13 @@
+package com.sonata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
